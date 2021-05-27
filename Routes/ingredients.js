9aee4d4b87608c9793b2b2f257d43d7ec1a48903
Aashir Azeem <jadude19@gmail.com>
2021-05-27 17:26:19 +0500

    edit route added

--- Routes/ingredients.js
+++ Routes/ingredients.js
@@ -94,4 +94,79 @@ routes.post("", async (req, res, next) => {
 
 });
 
+// Add New Ingredient data Route
+routes.put("", async (req, res, next) => {
+    // try {
+        
+        // images CDN URL
+        imagesUrl = {
+            image1: '',
+            image2: '',
+            image3: ''
+        }
+
+        if(req.body.fileSource1 != '') {
+            const fileStr = req.body.fileSource1;
+            const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+                upload_preset: 'ingredient',
+            });
+            imagesUrl.image1 = uploadResponse.url;
+        }
+
+        if(req.body.fileSource2 != '') {
+            const fileStr = req.body.fileSource2;
+            const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+                upload_preset: 'ingredient',
+            });
+            imagesUrl.image2 = uploadResponse.url;
+        }
+
+        if(req.body.fileSource3 != '') {
+            const fileStr = req.body.fileSource3;
+            const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+                upload_preset: 'ingredient',
+            });
+            imagesUrl.image3 = uploadResponse.url;
+        }
+        
+        // Query string
+        var sql = `UPDATE ingredient 
+        (ingredient, alt_ingredient_1, alt_ingredient_2, alt_ingredient_3, alt_ingredient_1_image, alt_ingredient_2_image, alt_ingredient_3_image) 
+        VALUES ( 
+            '${req.body.ingredient}', 
+            '${req.body.ingredientName1}', '${req.body.ingredientName2}', '${req.body.ingredientName3}', 
+            '${imagesUrl.image1}', '${imagesUrl.image2}', '${imagesUrl.image3}'
+        )`;
+
+        // connect database
+        connection.query(sql, function (err, result, fields) {
+            if (err) {
+                console.log('Error', err.sqlMessage, err.code === 'ER_DUP_ENTRY');
+                if(err.code === 'ER_DUP_ENTRY') {
+                    res.status(200).json({
+                        code: 502,
+                        message: err.sqlMessage,
+                    });
+                } else {
+                    res.status(200).json({
+                        code: 502,
+                        message: err.sqlMessage,
+                    });
+                }
+            } else {
+                console.log('result', result);
+
+                res.status(200).json({
+                    code: 200,
+                    message: "Ingredients added successfully!",
+                });
+            }
+        });
+    // } catch (err) {
+    //     console.error(err);
+    //     res.status(500).json({ message: err });
+    // }
+
+});
+
 module.exports = routes;
